f41d2d182bc8d0a755db3c524133622ab180a16b
doronkon <doronkon57@gmail.com>
2025-01-27 13:13:45 +0200

    added admin page

---
+++ app/src/main/java/com/example/catflix_android/Activities/AdminPageActivity.java
@@ -0,0 +1,38 @@
+package com.example.catflix_android.Activities;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.catflix_android.Fragments.HeaderFragment;
+import com.example.catflix_android.R;
+
+public class AdminPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_admin_page);
+
+        // Add the fragment dynamically to the container (header_container)
+        if (savedInstanceState == null) {
+            HeaderFragment headerFragment = new HeaderFragment();
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            transaction.replace(R.id.header_container, headerFragment); // Replace the container with the fragment
+            transaction.commit();
+        }
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
--- app/src/main/java/com/example/catflix_android/Fragments/HeaderFragment.java
+++ app/src/main/java/com/example/catflix_android/Fragments/HeaderFragment.java
@@ -7,8 +7,11 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 import androidx.fragment.app.Fragment;
+
+import com.example.catflix_android.Activities.AdminPageActivity;
 import com.example.catflix_android.Activities.HomePageActivity;
 import com.example.catflix_android.Activities.ProfileActivity;
+import com.example.catflix_android.DataManager;
 import com.example.catflix_android.R;
 
 public class HeaderFragment extends Fragment {
@@ -20,12 +23,15 @@ public class HeaderFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        boolean isAdmin = DataManager.getIsAdmin();
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_header, container, false);
 
         // Set up click listeners for "Home" and "Profile"
         TextView homeLink = rootView.findViewById(R.id.home_link);
         TextView profileLink = rootView.findViewById(R.id.profile_link);
+        TextView adminLink = rootView.findViewById(R.id.admin);
 
         homeLink.setOnClickListener(v -> {
             Intent homeIntent = new Intent(getActivity(), HomePageActivity.class);
@@ -36,6 +42,14 @@ public class HeaderFragment extends Fragment {
             Intent profileIntent = new Intent(getActivity(), ProfileActivity.class);
             startActivity(profileIntent);
         });
+        if(!isAdmin){
+            adminLink.setVisibility(View.VISIBLE);
+            adminLink.setOnClickListener(v -> {
+                Intent adminIntent = new Intent(getActivity(), AdminPageActivity.class);
+                startActivity(adminIntent);
+            });
+        }
+
 
         return rootView;
     }
